3e317c486606b1e24a3eabd7a015c0f96abd919f
alesizile <ai_love_love@mail.ru>
2014-03-14 18:20:38 +0400

    backbone fetch

--- assets/javascripts/intercom.js
+++ assets/javascripts/intercom.js
@@ -1,1 +1,28 @@
-console.log("test");
+$(function(){
+
+	console.log($("#simpleText"));
+	console.log("init");
+
+	var groupCollection = Backbone.Collection.extend({
+  		url: 'http://localhost:3000/groups.json'
+	});
+
+	var groupList = new groupCollection
+
+	groupList.fetch(
+		{
+			error: function(){
+				console.log("some errors");
+			},
+			success: function(){
+				console.log("it's ok");
+				//1 sublime plugin (beautify js)
+				//2 git aliases				
+				//3 fix it
+				//4 read: backbone view, model
+				//5 crlf configuration
+				//_.each(this.groupList, function(g){ console.log(g); });
+				$("#simpleText").html("put groups from redmine");
+			}
+	});
+});
