53b06693e15573cbd9339c6dd451f0125b242023
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2014-03-30 19:45:35 +0400

    Add caption translation

--- init.rb
+++ init.rb
@@ -1,10 +1,11 @@
 require 'redmine'
 
 Redmine::Plugin.register :intercom do
-  name 'Intercom plugin'
+  name 'InTercom plugin'
   author 'Vladislav R. and corp.'
-  description 'Votes'
+  description 'InTercom Social Network'
   version '1.0.0'
-  menu :top_menu, :intercom, { :controller => 'intercom', :action => 'index' }, :caption => 'intercom'
+  menu :top_menu, :intercom, { :controller => 'intercom', :action => 'index' },
+   :caption => :intercom_caption
   #Вывод плагина в главном меню redmine
 end
