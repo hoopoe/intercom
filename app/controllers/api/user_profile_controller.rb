35cdee5e0757e4cd14a9eb97b0bb2b2592281131
PolinaDurdeva <polina@citydom.ru>
2014-04-04 17:03:13 +0400

    Bags

--- app/controllers/api/user_profile_controller.rb
+++ app/controllers/api/user_profile_controller.rb
@@ -1,7 +1,23 @@
-class Api::UserProfileController < ApplicationController  
+class Api::UserProfileController < ApplicationController
   respond_to :json
-  def index        
-    @profiles = UserProfile.all
-    respond_with @profiles
+
+  def index    
+    if (params[:skills])
+      skills = params[:skills]
+      skillsT = skills.map{|s| "%#{s}%"}
+      Rails::logger::debug skillsT
+
+      q = UserProfile.arel_table
+      qanchor = 'skills LIKE ?'
+      (skills.count - 1).times do
+        qanchor += ' or skills LIKE ?'
+      end
+
+      conditions = skillsT.clone
+      conditions.insert(0, qanchor)
+      respond_with UserProfile.find(:all, :conditions => conditions)     
+    else
+      respond_with UserProfile.all
+    end
   end
 end
--- db/migrate/007_create_user_projects.rb
+++
@@ -1,7 +0,0 @@
-class CreateUserProjects < ActiveRecord::Migration
-  def change
-    create_table :user_projects do |t|
-      t.string :name
-    end
-  end
-end
