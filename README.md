76d9a41157c5f61e5db060f9046aa69a4d4abef8
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2014-04-14 23:29:43 +0400

    migration clean up

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Install Instructions: 
 Please, install plugin into "~/redmine/plugins/intercom"
 
-Use rake redmine:intercom:reset_data to load demo data
+Test data:
+Copy person.csv to redmine root
+Use *rake redmine:intercom:reset_data* to load demo data
--- db/migrate/001_create_user_profiles.rb
+++ db/migrate/001_create_user_profiles.rb
@@ -3,12 +3,16 @@ class CreateUserProfiles < ActiveRecord::Migration
     create_table :user_profile_t do |t|
       t.string :skills
       t.integer :user_id,          :default => 0,     :null => false
+      t.string :name
+      t.string :first_name
+      t.text :position
+      t.text :summary
+      t.string :birthday
+      t.text :project
     end
-
-    UserProfile.create(:user_id => 1, :skills => 'c++, java, ruby')
   end
 
-   def down
-   	drop_table:user_profile_t
-   end
+  def down
+    drop_table:user_profile_t
+  end
 end
