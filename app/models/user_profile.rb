cb7498d4225fd041da0230fcc62b646e4139ec9f
alesizile <ai_love_love@mail.ru>
2014-04-22 17:20:38 +0400

    smthn

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -2,7 +2,7 @@ class UserProfile < ActiveRecord::Base
   self.table_name = "user_profile_t"
 
   belongs_to :user
-  has_and_belongs_to_many :user_projects
+  #has_and_belongs_to_many :user_projects
   
   def to_s
     "Name: empty Skills:#{self.skills}"
