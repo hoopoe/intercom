33367ad8f2e434e528f2f8caca99a4c83e2a417a
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2014-05-22 15:18:18 +0400

    use offset and limit inside api

--- app/controllers/api/user_profile_controller.rb
+++ app/controllers/api/user_profile_controller.rb
@@ -17,11 +17,17 @@ class Api::UserProfileController < ApplicationController
       @users = User.select("users.id, users.login, users.mail, users.firstname, users.lastname, user_profile_t.skills")
       .joins("LEFT JOIN #{UserProfile.table_name} ON #{User.table_name}.id = #{UserProfile.table_name}.user_id")  
       .where("LOWER(#{UserProfile.table_name}.skills) LIKE LOWER(?)", q)
-      respond_with @users.order(sort_clause)
+      respond_with @users.order(sort_clause).
+                          limit(@limit).
+                          offset(@offset).
+                          all
     else
       @users = User.select("users.id, users.login, users.mail, users.firstname, users.lastname, user_profile_t.skills")
       .joins("LEFT JOIN #{UserProfile.table_name} ON #{User.table_name}.id = #{UserProfile.table_name}.user_id")  
-      respond_with @users.order(sort_clause)
+      respond_with @users.order(sort_clause).
+                          limit(@limit).
+                          offset(@offset).
+                          all
     end    
   end
 
