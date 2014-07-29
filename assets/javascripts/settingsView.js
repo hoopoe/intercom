f483f4ae09d69dbc91b4eca546673fd6bf7acda2
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2014-07-29 13:33:01 +0400

    add base settings view impl.

--- assets/javascripts/settingsView.js
+++ assets/javascripts/settingsView.js
@@ -2,12 +2,19 @@ var app = app || {};
 
 $(function() {
 
-    app.settingsTemplate = $("#settings-template").html();
-
     SettingsView = Backbone.View.extend({
 
+        template: _.template($("#settings-template").html()),
+
         initialize: function() {
-            this.$el.html('TBD');
+            // this.$el.html('TBD');
+            this.$el.html('');
+            this.$el.html(this.template(this.model.toJSON()));
+            $('.dropdown-menu li').click(function(e) {
+                console.log("tt");
+                e.preventDefault();
+            });
+            // console.log($('.dropdown-menu li'));
         },
 
         render: function() {
