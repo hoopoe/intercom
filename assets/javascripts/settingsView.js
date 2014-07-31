bd2a6215af3aa3b4aeb9aa61627b157c6126b5d8
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2014-07-31 14:47:30 +0400

    setting partial impl.

--- assets/javascripts/settingsView.js
+++ assets/javascripts/settingsView.js
@@ -6,19 +6,35 @@ $(function() {
 
         template: _.template($("#settings-template").html()),
 
+        events: {
+            'click .dropdown-menu > li > a': 'selectTheme'
+        },
+
         initialize: function() {
-            // this.$el.html('TBD');
             this.$el.html('');
             this.$el.html(this.template(this.model.toJSON()));
-            $('.dropdown-menu li').click(function(e) {
-                console.log("tt");
-                e.preventDefault();
-            });
+
+            if (this.model.get('user')) {
+                console.log(this.model.get('user'));
+            } else {
+                console.log("don't have settings");
+            }
+
+            // $('.dropdown-menu li').click(function(e) {
+            //     console.log("tt");
+            //     e.preventDefault();
+            // });
             // console.log($('.dropdown-menu li'));
         },
 
         render: function() {
             return this;
+        },
+
+        selectTheme: function(e) {
+            e.preventDefault();
+            // window.tt = e;
+            console.log(e.currentTarget.getAttribute('value'));
         }
     });
 });
