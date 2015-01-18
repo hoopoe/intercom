ad1980b69a9e5c290daad88e40ce8afa99fc41a8
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2015-01-18 21:59:48 +0300

    ignore old migration related to paperclip

--- db/migrate/20140602140230_add_attachment_avatar_to_userprofiles.rb
+++ db/migrate/20140602140230_add_attachment_avatar_to_userprofiles.rb
@@ -1,11 +1,12 @@
 class AddAttachmentAvatarToUserprofiles < ActiveRecord::Migration
+	#paperclip removed
   def self.up
-    change_table :user_profile_t do |t|
-      t.attachment :avatar
-    end
+    # change_table :user_profile_t do |t|
+    #   t.attachment :avatar
+    # end
   end
 
   def self.down
-    drop_attached_file :user_profile_t, :avatar
+    # drop_attached_file :user_profile_t, :avatar
   end
 end
