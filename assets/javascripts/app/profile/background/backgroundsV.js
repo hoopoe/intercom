41926499cb60228a90956c69f81b419643ce67e5
Kocheryzhkin Vladimir <vkocherizhkin@gmail.com>
2015-03-05 12:01:13 +0300

    cosmetic changes

--- assets/javascripts/app/profile/background/backgroundsV.js
+++ assets/javascripts/app/profile/background/backgroundsV.js
@@ -1,21 +1,35 @@
 define([
     'app/profile/background/backgroundV'
 ], function(BackgroundView) {
-  var view = Backbone.View.extend({
-    tagName: 'ul',
-    className: 'backgrounds-view',
-    editable: false,        
-    setEditable:function(e) { this.editable = e; },
-    render: function() {            
-        this.collection.each(function(background) {
-            background.set('editable', this.editable);                
-            var backgroundView = new BackgroundView({
-                model: background                    
-            });
-            this.$el.append(backgroundView.render().el);
-        }, this);            
-        return this;
-    }
-  });
-  return view
+    var view = Backbone.View.extend({
+        tagName: 'ul',
+        className: 'backgrounds-view',
+        editable: false,
+        setEditable: function(e) {
+            this.editable = e;
+        },
+        render: function() {
+            this.collection.comparator = function(a, b) {
+                var left = a.get("to").substr(a.get("to").lastIndexOf(" "));
+                var right = b.get("to").substr(b.get("to").lastIndexOf(" "));                              
+                if (left > right) {
+                    return -1;
+                }
+                if (right > left) {
+                    return 1;
+                }
+                return 0;
+            };
+            this.collection.sort();            
+            this.collection.each(function(background) {
+                background.set('editable', this.editable);
+                var backgroundView = new BackgroundView({
+                    model: background
+                });
+                this.$el.append(backgroundView.render().el);
+            }, this);
+            return this;
+        }
+    });
+    return view
 });
--- assets/javascripts/app/profile/position/positionsV.js
+++ assets/javascripts/app/profile/position/positionsV.js
@@ -1,23 +1,35 @@
 define([
-  'app/profile/position/positionV'
+    'app/profile/position/positionV'
 ], function(PositionView) {
-  var view = Backbone.View.extend({
-    tagName: 'ul',
-    className: 'positions-view',
-    editable: false,        
-    setEditable:function(e) {
-        this.editable = e;            
-    },
-    render: function() {
-        this.collection.each(function(position) {
-            position.set('editable', this.editable);                
-            var positionView = new PositionView({
-                model: position                    
-            });
-            this.$el.append(positionView.render().el);
-        }, this);            
-        return this;
-    }
-  });
-  return view
+    var view = Backbone.View.extend({
+        tagName: 'ul',
+        className: 'positions-view',
+        editable: false,
+        setEditable: function(e) {
+            this.editable = e;
+        },
+        render: function() {
+            this.collection.comparator = function(a, b) {
+                var left = a.get("to").substr(a.get("to").lastIndexOf(" "));
+                var right = b.get("to").substr(b.get("to").lastIndexOf(" "));
+                if (left > right) {
+                    return -1;
+                }
+                if (right > left) {
+                    return 1;
+                }
+                return 0;
+            };
+            this.collection.sort();
+            this.collection.each(function(position) {
+                position.set('editable', this.editable);
+                var positionView = new PositionView({
+                    model: position
+                });
+                this.$el.append(positionView.render().el);
+            }, this);
+            return this;
+        }
+    });
+    return view
 });
